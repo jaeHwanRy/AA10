00a70dce1b362f20e7c7e70dae832e8b7f6d3216
jaeHwanRy <112610418+jaeHwanRy@users.noreply.github.com>
2022-09-08 16:33:00 +0900

    Add files via upload

---
+++ aa10_rpt02/start/hello_function.js
@@ -0,0 +1,6 @@
+function hello(what){
+    console.log("Hello " + what +" !");
+}
+
+hello("aa10");
+hello("류재환");
---
+++ aa10_rpt02/start/index_aa10.js
@@ -0,0 +1,5 @@
+var myinfo = require('./aa10info');
+
+myinfo("aa10","류재환","010-8181-5316");
+
+myinfo("aa99","이주영","010-1234-5678")
